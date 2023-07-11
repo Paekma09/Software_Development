d84c2f10fb5d608bb136db9de4530076b7b261b3
KJ Nam <paekma09@ibizsoftware.net>
2023-07-11 10:53:49 +0900

    4-4

---
+++ src/main/java/chapter_04/DocumentManagementSystem.java
@@ -0,0 +1,14 @@
+package main.java.chapter_04;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DocumentManagementSystem {
+    private final Map<String, Importer> extensionToImporter = new HashMap<>();
+
+    public DocumentManagementSystem() {
+        extensionToImporter.put("letter", new LetterImporter());
+        extensionToImporter.put("report", new ReportImporter());
+        extensionToImporter.put("jpg", new ImageImporter());
+    }
+}
---
+++ src/main/java/chapter_04/LetterImporter.java
@@ -0,0 +1,11 @@
+package main.java.chapter_04;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LetterImporter implements Importer{
+    @Override
+    public Document importFile(File file) throws IOException {
+        return null;
+    }
+}
