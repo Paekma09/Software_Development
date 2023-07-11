a031d43770b65824d838e74750cc79fbe6a230e0
KJ Nam <paekma09@ibizsoftware.net>
2023-07-11 13:25:31 +0900

    4-7

--- src/main/java/chapter_04/InvoiceImporter.java
+++ src/main/java/chapter_04/InvoiceImporter.java
@@ -6,19 +6,19 @@ import java.util.Map;
 
 import static main.java.chapter_04.Attributes.*;
 
-public class LetterImporter implements Importer{
+public class InvoiceImporter implements Importer {
     private static final String NAME_PREFIX = "Dear";
+    private static final String AMOUNT_PREFIX = "Amount: ";
+
     @Override
     public Document importFile(final File file) throws IOException {
         final TextFile textFile = new TextFile(file);
 
         textFile.addLineSuffix(NAME_PREFIX, PATIENT);
-
-        final int lineNumber = textFile.addLines(2, String::isEmpty, ADDRESS);
-        textFile.addLines(lineNumber + 1, (line) -> line.startsWith("regards,"), BODY);
+        textFile.addLineSuffix(AMOUNT_PREFIX, AMOUNT);
 
         final Map<String, String> attributes = textFile.getAttributes();
-        attributes.put(TYPE, "LETTER");
+        attributes.put(TYPE, "INVOIVCE");
         return new Document(attributes);
     }
 }
